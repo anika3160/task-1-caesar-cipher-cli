363e5e737e9d22ad54adb8721547706cc8fc1a9e
Anna Surahatava <anika3160@gmail.com>
2021-05-04 21:43:04 +0300

    feat: add test data

---
+++ src/testData.js
@@ -0,0 +1,19 @@
+const inputString = `This is secret. Message about "_" symbol!`;
+const outputString= 'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!';
+const isEncode = true;
+const shift = 7;
+
+const inputString= 'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!';
+const outputString= `This is secret. Message about "_" symbol!`;
+const isEncode = false;
+const shift = 7;
+
+const inputString = `This is secret. Message about "_" symbol!`;
+const outputString= 'Sghr hr rdbqds. Ldrrzfd zants "_" rxlank!';
+const isEncode = true;
+const shift = -1;
+
+const inputString = `Sghr hr rdbqds. Ldrrzfd zants "_" rxlank!`;
+const outputString= 'This is secret. Message about "_" symbol!';
+const isEncode = false;
+const shift = -1;
