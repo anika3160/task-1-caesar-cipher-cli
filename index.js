9e61e968323acb08346d34c38535ee9e366ad6dc
Anna Surahatava <anika3160@gmail.com>
2021-05-05 20:55:54 +0300

    fix: eslint problems

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ import { getArrayFromString } from './src/helpers.js';
 const ALPHABET_LENGTH = ALPHABETS.small.length;
 
 // parse data from console
-let rawArguments=process.argv.slice(2);
+const rawArguments = process.argv.slice(2);
 const parserOptions = {
-    alias: {
-        s: 'shift',
-        i: 'input',
-        o: 'output',
-        a: 'action',
-    },
-    string: ['a', 'action'],
-    unknown: (arg) => {
-        console.error('Unknown option: ', arg);
-        console.log(`
+  alias: {
+    s: 'shift',
+    i: 'input',
+    o: 'output',
+    a: 'action',
+  },
+  string: ['a', 'action'],
+  unknown: (arg) => {
+    console.error('Unknown option: ', arg);
+    console.log(`
 CLI tool accept 4 options (short alias and full name):
     -s, --shift: a shift
     -i, --input: an input file
@@ -26,11 +26,11 @@ CLI tool accept 4 options (short alias and full name):
     -a, --action: an action encode/decode.
 Please use only accept options!
 `);
-        return false
-      }
+    return false;
+  },
 };
 
-let consoleArg = minimist(rawArguments, parserOptions);
+const consoleArg = minimist(rawArguments, parserOptions);
 
 const { shift, action } = consoleArg;
 const isEncode = (action === 'encode');
@@ -38,33 +38,34 @@ const isEncode = (action === 'encode');
 console.log(consoleArg);
 
 // test data
-const inputString = `This is secret. Message about "_" symbol!`;
-const outputString= 'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!';
+const inputString = 'This is secret. Message about "_" symbol!';
+const outputString = 'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!';
 
 // begin
 const getNewSymbolFromAlphabet = (alphabet, currentEl, isEncode) => {
-    const alphaArray = getArrayFromString(alphabet);
-    const index = alphaArray.indexOf(currentEl);
-    if (index !== -1) {
-        const replaceElIndex = isEncode ? (ALPHABET_LENGTH+index+shift) % ALPHABET_LENGTH : (ALPHABET_LENGTH-shift+index) % ALPHABET_LENGTH;
-        
-        return alphaArray[replaceElIndex];
-    }
-    return index;
-}
+  const alphaArray = getArrayFromString(alphabet);
+  const index = alphaArray.indexOf(currentEl);
+  if (index !== -1) {
+    const replaceElIndex = isEncode
+      ? (ALPHABET_LENGTH + index + shift) % ALPHABET_LENGTH
+      : (ALPHABET_LENGTH - shift + index) % ALPHABET_LENGTH;
+
+    return alphaArray[replaceElIndex];
+  }
+  return index;
+};
 
 const getOutputString = (isEncode = true) => {
-    const finishArray = getArrayFromString(inputString).map((el) => {
-        let secretEl = getNewSymbolFromAlphabet(ALPHABETS.small, el, isEncode);
-        if (secretEl == -1) {
-            secretEl = getNewSymbolFromAlphabet(ALPHABETS.big, el, isEncode);
-            if (secretEl == -1) return el;
-        }
-        return secretEl;
-    });
+  const finishArray = getArrayFromString(inputString).map((el) => {
+    let secretEl = getNewSymbolFromAlphabet(ALPHABETS.small, el, isEncode);
+    if (secretEl === -1) {
+      secretEl = getNewSymbolFromAlphabet(ALPHABETS.big, el, isEncode);
+      if (secretEl === -1) return el;
+    }
+    return secretEl;
+  });
 
-    return finishArray.join('');
-}
-
+  return finishArray.join('');
+};
 
 console.log(getOutputString(isEncode));
