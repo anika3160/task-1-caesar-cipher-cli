562cd1334e28cde9e7bd7a83ada9679491b8fad3
Anna Surahatava <anika3160@gmail.com>
2021-05-05 18:42:17 +0300

    feat: parse console options

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
+import minimist from 'minimist';
+
 import { ALPHABETS } from './src/constants.js';
 import { getArrayFromString } from './src/helpers.js';
 
 // data
 const ALPHABET_LENGTH = ALPHABETS.small.length;
 
+// parse data from console
+let rawArguments=process.argv.slice(2);
+const parserOptions = {
+    alias: {
+        s: 'shift',
+        i: 'input',
+        o: 'output',
+        a: 'action',
+    },
+    string: ['a', 'action'],
+    unknown: (arg) => {
+        console.error('Unknown option: ', arg);
+        console.log(`
+CLI tool accept 4 options (short alias and full name):
+    -s, --shift: a shift
+    -i, --input: an input file
+    -o, --output: an output file
+    -a, --action: an action encode/decode.
+Please use only accept options!
+`);
+        return false
+      }
+};
+
+let consoleArg = minimist(rawArguments, parserOptions);
+
+const { shift, action } = consoleArg;
+const isEncode = (action === 'encode');
+
+console.log(consoleArg);
+
 // test data
 const inputString = `This is secret. Message about "_" symbol!`;
 const outputString= 'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!';
-const isEncode = true;
-const shift = 7;
 
 // begin
 const getNewSymbolFromAlphabet = (alphabet, currentEl, isEncode) => {
