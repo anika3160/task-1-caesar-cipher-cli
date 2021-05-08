39d75fed2c8ae9137ffb85546615cd45beb783a1
Anna Surahatava <anika3160@gmail.com>
2021-05-08 14:44:32 +0300

    feat: add help message

--- src/consoleWorker.js
+++ src/consoleWorker.js
@@ -7,7 +7,7 @@ export const writeToConsole = (outputStr, callback) => {
 };
 
 export const readFromConsole = async (output, shift, isEncode) => {
-  process.stderr.write('You do not provide a path to the input file. Please write your message here: ');
+  process.stderr.write('You do not provide a path to the input file. Please write your message here:\n(If you want to exit press Ctrl + C)\n');
   process.stdin.setEncoding('utf8');
   process.stdin.resume();
   process.stdin.on('readable', async () => {
