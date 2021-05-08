cbfca984f69250b23a304b07e09e6c5da5da709b
Anna Surahatava <anika3160@gmail.com>
2021-05-08 11:59:52 +0300

    feat: update file work functions

--- src/fileWorker.js
+++ src/fileWorker.js
@@ -1,22 +1,31 @@
 import fs from 'fs';
 
 import getTransformedString from './transformator.js';
+import { JS_DATA_TYPES } from './constants.js';
 
-const fileWorker = async (input, output, shift, isEncode) => {
+export const fileReader = (input, callback) => {
   fs.readFile(input, 'utf8', (error, inputString) => {
     if (error) {
-      console.log(error);
-    } else if (typeof inputString === 'string' && inputString.length > 0) {
-      fs.appendFile(output, getTransformedString(`${inputString}\n`, shift, isEncode), (err) => {
-        if (err) {
-          console.log(error);
-        } else {
-          console.log('Запись файла завершена. Содержимое файла:');
-          console.log(fs.readFileSync(output, 'utf8'));
-        }
-      });
+      process.stderr.write('Input file is undefined or unreadable. Please try again with all required params.');
+      process.exit(-1);
+    } else if (typeof inputString === JS_DATA_TYPES.string) {
+      callback(inputString);
     }
   });
 };
 
-export default fileWorker;
+export const fileWriter = (inputStr, output, shift, isEncode) => {
+  fs.appendFile(output, getTransformedString(`${inputStr}\n`, shift, isEncode), (err) => {
+    if (err) {
+      process.stderr.write('Output file is undefined or unreadable. Please try again with all required params.');
+      process.exit(-1);
+    } else {
+      process.stdout.write('Output file:');
+      process.stdout.write(fs.readFileSync(output, 'utf8'));
+    }
+  });
+};
+
+export const fileWorker = (input, output, shift, isEncode) => {
+  fileReader(input, (inputStr) => fileWriter(inputStr, output, shift, isEncode));
+};
